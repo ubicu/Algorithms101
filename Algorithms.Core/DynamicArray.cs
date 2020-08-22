9b1d1b4d99b7a7b9bbb577067d2b04ece6b957f5
Ham Rara <hmrara01@gmail.com>
2020-08-22 18:04:09 -0400

    Adding more features to DynamicArray and adding/updating corresponding unit tests

--- Algorithms.Core/DynamicArray.cs
+++ Algorithms.Core/DynamicArray.cs
@@ -1,14 +1,29 @@
 ﻿using System;
+using System.Drawing;
 
 namespace Algorithms.Core
 {
     public class DynamicArray<T>
     {
+
+        #region Array Notes
+        /*
+            Get/Set - O(1)
+            Insert  - O(n)
+            Delete  - O(n)
+
+            Killer Features:
+            (1) Random access
+            (2) Fixed capacity - use dynamic array implementation to automatically resize
+            (3) Double when resize
+        */
+        #endregion
+
         private T[] _data;
         private int _size;
         private int _initialCapacity;
 
-        public DynamicArray(int initialCapacity)
+        public DynamicArray(int initialCapacity = 10)
         {
             this._initialCapacity = initialCapacity;
             this._data = new T[initialCapacity];
@@ -23,5 +38,84 @@ namespace Algorithms.Core
         {
             _data[index] = value;
         }
+
+        public void Insert(int index, T value)
+        {
+            // Check size
+            if (_size == _initialCapacity)
+            {
+                resize();
+            }
+
+            // Copy up
+            for(int j = _size; j > index; j--)
+            {
+                _data[j] = _data[j - 1];
+            }
+
+            // Insert
+            _data[index] = value;
+            _size++;
+        }
+
+        public void Add(T value)
+        {
+            if (_size == _initialCapacity)
+            {
+                resize();
+            }
+
+            _data[_size] = value;
+            _size++;
+        }
+
+        public int Size()
+        {
+            return _size;
+        }
+
+        public void Delete(int index)
+        {
+            _data[index] = default(T);
+
+            // Copy down
+            for(int j = index; j < _size; j++)
+            {
+                _data[j] = _data[j + 1];
+            }
+
+            _size--;
+        }
+
+        public bool IsEmpty()
+        {
+            return (_size == 0);
+        }
+
+        public bool Contains(T value)
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                if (_data[i].Equals(value))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        #region Private methods
+        private void resize()
+        {
+            T[] newData = new T[_initialCapacity * 2];
+            for (int i = 0; i < _initialCapacity; i++)
+            {
+                newData[i] = _data[i];
+            }
+
+            _data = newData;
+            _initialCapacity = _initialCapacity * 2;
+        }
+        #endregion
     }
 }
--- Algorithms.Test/DynamicArrayTest.cs
+++ Algorithms.Test/DynamicArrayTest.cs
@@ -21,5 +21,89 @@ namespace Algorithms.Test
             Assert.Equal("a", dynamicArray.Get(0));
         }
 
+        [Fact]
+        public void Insert()
+        {
+            dynamicArray.Add("a");
+            dynamicArray.Add("b");
+            dynamicArray.Add("c");
+
+            dynamicArray.Insert(index: 1, value: "d");
+
+            Assert.Equal(4, dynamicArray.Size());
+            Assert.Equal("a", dynamicArray.Get(0));
+            Assert.Equal("d", dynamicArray.Get(1));
+            Assert.Equal("b", dynamicArray.Get(2));
+            Assert.Equal("c", dynamicArray.Get(3));
+        }
+
+        [Fact]
+        public void DeleteFirstElement()
+        {
+            dynamicArray.Add("a");
+            dynamicArray.Add("b");
+            dynamicArray.Add("c");
+
+            dynamicArray.Delete(index: 0);
+
+            Assert.Equal(2, dynamicArray.Size());
+            Assert.Equal("b", dynamicArray.Get(0));
+            Assert.Equal("c", dynamicArray.Get(1));
+            Assert.Null(dynamicArray.Get(2));
+        }
+
+        [Fact]
+        public void DeleteMiddleElement()
+        {
+            dynamicArray.Add("a");
+            dynamicArray.Add("b");
+            dynamicArray.Add("c");
+
+            dynamicArray.Delete(index: 1);
+
+            Assert.Equal(2, dynamicArray.Size());
+            Assert.Equal("a", dynamicArray.Get(0));
+            Assert.Equal("c", dynamicArray.Get(1));
+            Assert.Null(dynamicArray.Get(2));
+        }
+
+        [Fact]
+        public void DeleteLastElement()
+        {
+            dynamicArray.Add("a");
+            dynamicArray.Add("b");
+            dynamicArray.Add("c");
+
+            dynamicArray.Delete(index: 2);
+
+            Assert.Equal(2, dynamicArray.Size());
+            Assert.Equal("a", dynamicArray.Get(0));
+            Assert.Equal("b", dynamicArray.Get(1));
+            Assert.Null(dynamicArray.Get(2));
+        }
+
+        [Fact]
+        public void IsEmpty()
+        {
+            Assert.True(dynamicArray.IsEmpty());
+
+            dynamicArray.Add("a");
+
+            Assert.False(dynamicArray.IsEmpty());
+        }
+
+        [Fact]
+        public void Contains()
+        {
+            Assert.False(dynamicArray.Contains("a"));
+
+            dynamicArray.Add("a");
+            Assert.True(dynamicArray.Contains("a"));
+
+            dynamicArray.Delete(0);
+
+            Assert.False(dynamicArray.Contains("a"));
+
+        }
     }
 }
