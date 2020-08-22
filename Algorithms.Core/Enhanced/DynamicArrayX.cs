568690beb18bee7480b219494b49193f3bbc745e
Ham Rara <hmrara01@gmail.com>
2020-08-22 19:39:49 -0400

    Checking-in code for DynamicArrayX, an enhanced version of DynamicArray, in terms of argument and index validations. Unit tests associated with DynamicArrayX are also included.

---
+++ Algorithms.Core/Enhanced/DynamicArrayX.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Core
+{
+    /*
+     * Improved implementation for DynamicArray 
+     * with argument and index validation
+     */
+    public class DynamicArrayX<T>
+    {
+        #region Array Notes
+        /*
+            Get/Set - O(1)
+            Insert  - O(n)
+            Delete  - O(n)
+
+            Killer Features:
+            (1) Random access
+            (2) Fixed capacity - use dynamic array implementation to automatically resize
+            (3) Double when resize
+        */
+        #endregion
+
+        private T[] _data;
+        private int _size;
+        private int _initialCapacity;
+
+        public DynamicArrayX(int initialCapacity = 10)
+        {
+            if (initialCapacity < 0)
+            {
+                throw new ArgumentOutOfRangeException("Initial capacity must be non-negative");
+            }
+
+            this._initialCapacity = initialCapacity;
+            this._data = new T[initialCapacity];
+        }
+
+        public T Get(int index)
+        {
+            if (index < 0 || index >= _size)
+            {
+                throw new IndexOutOfRangeException($"Index must be non-negative and less than size (currently at {_size} of dynamic array.");
+            }
+
+            return _data[index];
+        }
+
+        public void Set(int index, T value)
+        {
+            if (index < 0 || index >= _size)
+            {
+                throw new IndexOutOfRangeException($"Index must be non-negative and less than size (currently at {_size} of dynamic array.");
+            }
+
+            _data[index] = value;
+        }
+
+        public void Add(T value)
+        {
+            if (_size == _initialCapacity)
+            {
+                resize();
+            }
+
+            _data[_size] = value;
+            _size++;
+        }
+
+        public int Size()
+        {
+            return _size;
+        }
+
+        public void Insert(int index, T value)
+        {
+            if (index < 0 || index >= _size)
+            {
+                throw new IndexOutOfRangeException($"Index must be non-negative and less than size (currently at {_size} of dynamic array.");
+            }
+
+            // Check size
+            if (_size == _initialCapacity)
+            {
+                resize();
+            }
+
+            // Copy up
+            for (int j = _size; j > index; j--)
+            {
+                _data[j] = _data[j - 1];
+            }
+
+            // Insert
+            _data[index] = value;
+            _size++;
+        }
+
+        public void Delete(int index)
+        {
+            if (index < 0 || index >= _size)
+            {
+                throw new IndexOutOfRangeException($"Index must be non-negative and less than size (currently at {_size} of dynamic array.");
+            }
+
+            _data[index] = default(T);
+
+            // Copy down
+            for (int j = index; j < _size; j++)
+            {
+                _data[j] = _data[j + 1];
+            }
+
+            _size--;
+        }
+
+        #region Private methods
+        private void resize()
+        {
+            T[] newData = new T[_initialCapacity * 2];
+            for (int i = 0; i < _initialCapacity; i++)
+            {
+                newData[i] = _data[i];
+            }
+
+            _data = newData;
+            _initialCapacity = _initialCapacity * 2;
+        }
+        #endregion
+    }
+}
---
+++ Algorithms.Test/Enhanced/DynamicArrayXTest.cs
@@ -0,0 +1,149 @@
+﻿using Algorithms.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Algorithms.Test
+{
+    public class DynamicArrayXTest
+    {
+        [Fact]
+        public void Initialization_ThrowsException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new DynamicArrayX<string>(-1));
+        }
+
+        [Fact]
+        public void Get_ThrowsException_EmptyArray()
+        {
+            var dynamicArray = new DynamicArrayX<string>();
+
+            Assert.Throws<IndexOutOfRangeException>(() => dynamicArray.Get(0));
+        }
+
+        [Fact]
+        public void Get_SuccessfulOperation()
+        {
+            var dynamicArray = new DynamicArrayX<string>();
+
+            dynamicArray.Add("a");
+            dynamicArray.Add("b");
+
+            Assert.Equal("a", dynamicArray.Get(0));
+            Assert.Equal("b", dynamicArray.Get(1));
+        }
+
+        [Fact]
+        public void Get_ThrowsException_IndexOutOfBounds()
+        {
+            var dynamicArray = new DynamicArrayX<string>();
+
+            dynamicArray.Add("a");
+            dynamicArray.Add("b");
+
+            Assert.Throws<IndexOutOfRangeException>(() => dynamicArray.Get(-1));
+            Assert.Throws<IndexOutOfRangeException>(() => dynamicArray.Get(2));
+        }
+
+        [Fact]
+        public void Set_ThrowsException_EmptyArray()
+        {
+            string insertValue = "a";
+
+            var dynamicArray = new DynamicArrayX<string>();
+
+            Assert.Throws<IndexOutOfRangeException>(() => dynamicArray.Set(0, insertValue));
+        }
+
+        [Fact]
+        public void Set_SuccessfulOperation()
+        {
+            var dynamicArray = new DynamicArrayX<string>();
+
+            dynamicArray.Add("a");
+            dynamicArray.Add("b");
+
+            dynamicArray.Set(0, "c");
+            dynamicArray.Set(1, "d");
+
+            Assert.Equal("c", dynamicArray.Get(0));
+            Assert.Equal("d", dynamicArray.Get(1));
+        }
+
+        [Fact]
+        public void Set_ThrowsException_IndexOutOfBounds()
+        {
+            var dynamicArray = new DynamicArrayX<string>();
+
+            dynamicArray.Add("a");
+            dynamicArray.Add("b");
+
+            Assert.Throws<IndexOutOfRangeException>(() => dynamicArray.Set(-1, "c"));
+            Assert.Throws<IndexOutOfRangeException>(() => dynamicArray.Set(2, "d"));
+        }
+
+        [Fact]
+        public void Insert_ThrowsException_IndexOutOfBounds()
+        {
+            var dynamicArray = new DynamicArrayX<string>();
+
+            dynamicArray.Add("a");
+            dynamicArray.Add("b");
+
+            Assert.Throws<IndexOutOfRangeException>(() => dynamicArray.Insert(-1, "c"));
+            Assert.Throws<IndexOutOfRangeException>(() => dynamicArray.Insert(2, "d"));
+        }
+
+        [Fact]
+        public void Insert_SuccessfulOperation()
+        {
+            var dynamicArray = new DynamicArrayX<string>();
+
+            dynamicArray.Add("a");
+            dynamicArray.Add("b");
+            dynamicArray.Insert(0, "c");
+
+            Assert.Equal(3, dynamicArray.Size());
+            Assert.Equal("c", dynamicArray.Get(0));
+            Assert.Equal("a", dynamicArray.Get(1));
+            Assert.Equal("b", dynamicArray.Get(2));
+        }
+
+        [Fact]
+        public void Delete_ThrowsException_IndexOutOfBounds()
+        {
+            var dynamicArray = new DynamicArrayX<string>();
+
+            dynamicArray.Add("a");
+            dynamicArray.Add("b");
+
+            Assert.Throws<IndexOutOfRangeException>(() => dynamicArray.Delete(-1));
+            Assert.Throws<IndexOutOfRangeException>(() => dynamicArray.Delete(2));
+        }
+
+        [Fact]
+        public void Delete_SuccessfulOperation()
+        {
+            var dynamicArray = new DynamicArrayX<string>();
+
+            dynamicArray.Add("a");
+            dynamicArray.Add("b");
+            dynamicArray.Add("c");
+
+            dynamicArray.Delete(0);
+
+            Assert.Equal(2, dynamicArray.Size());
+            Assert.Equal("b", dynamicArray.Get(0));
+            Assert.Equal("c", dynamicArray.Get(1));
+            Assert.Throws<IndexOutOfRangeException>(() => dynamicArray.Get(2));
+
+            dynamicArray.Delete(1);
+            Assert.Equal(1, dynamicArray.Size());
+            Assert.Equal("b", dynamicArray.Get(0));
+            Assert.Throws<IndexOutOfRangeException>(() => dynamicArray.Get(1));
+            Assert.Throws<IndexOutOfRangeException>(() => dynamicArray.Get(2));
+
+        }
+    }
+}
