4bc1cc288dca5eadeb60977fca16175b06bb3bb9
Ham Rara <hmrara01@gmail.com>
2020-08-23 16:33:32 -0400

    Created folders for the basic implementation of data structures. Added more features for linked lists as well as their associated unit tests.

---
+++ Algorithms.Core/DataStructures/LinkedList.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Core
+{
+    public class LinkedList<T>
+    {
+        #region LinkedList Notes
+        /*
+          Add front - O(1)
+          Add back  - O(n)
+          Delete    - O(n)
+
+          Disadvantages:
+          (a) no random access, unlike array
+
+          Advantages:
+          (a) no fixed capacity
+          (b) always the right size
+        */
+        #endregion
+
+        #region Node class
+        private class Node<T>
+        {
+            public T data { get; private set; }
+            public Node<T> Next { get; set; }
+
+            public Node(T data)
+            {
+                this.data = data;
+            }
+        }
+        #endregion
+
+        private Node<T> _head;
+
+        public void AddFront(T data)
+        {
+            // Create new node
+            var newNode = new Node<T>(data);
+
+            // If linked list is empty
+            if (_head == null)
+            {
+                _head = newNode;
+                return;
+            }
+
+            // Set new node next to the current head
+            newNode.Next = _head;
+
+            // Set current head to new node
+            _head = newNode;
+        }
+
+        public void AddBack(T data)
+        {
+            // Create new node
+            var newNode = new Node<T>(data);
+
+            // If list is empty
+            if (_head == null)
+            {
+                _head = newNode;
+                return;
+            }
+
+            // Iterate until the end of list
+            Node<T> current = _head;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+
+            // Point current to new node
+            current.Next = newNode;
+        }
+
+        public T GetFirst()
+        {
+            return _head.data;
+        }
+
+        public T GetLast()
+        {
+            if (_head == null)
+            {
+                throw new InvalidOperationException("The list is empty.");
+            }
+
+            Node<T> current = _head;
+            while(current.Next != null)
+            {
+                current = current.Next;
+            }
+
+            return current.data;
+        }
+
+        public int Size()
+        {
+            if (_head == null)
+            {
+                return 0;
+            }
+
+            int size = 1;
+            Node<T> current = _head;
+            while (current.Next != null)
+            {
+                current = current.Next;
+                size++;
+            }
+
+            return size;
+        }
+
+        public void Clear()
+        {
+            _head = null;
+        }
+
+        public void DeleteValue(T value)
+        {
+            if (_head == null)
+            {
+                return;
+            }
+
+            // Head contains data
+            if (_head.data.Equals(value))
+            {
+                _head = _head.Next;
+                return;
+            }
+
+            // Iterate until the end of list or value is found
+            Node<T> current = _head;
+            while (current.Next != null)
+            {
+                if (current.Next.data.Equals(value))
+                {
+                    current.Next = current.Next.Next;
+                    return;
+                }
+                current = current.Next;
+            }
+        }
+    }
+}
---
+++ Algorithms.Test/DataStructures/LinkedListTest.cs
@@ -0,0 +1,118 @@
+﻿
+using Xunit;
+using Algorithms.Core;
+
+namespace Algorithms.Test
+{
+    public class LinkedListTest
+    {
+        [Fact]
+        public void AddFront()
+        {
+            var linkedList = new LinkedList<int>();
+
+            linkedList.AddFront(1);
+            linkedList.AddFront(2);
+            linkedList.AddFront(3);
+
+            Assert.Equal(3, linkedList.GetFirst());
+            Assert.Equal(1, linkedList.GetLast());
+        }
+
+        [Fact]
+        public void GetFirst()
+        {
+            var linkedList = new LinkedList<int>();
+            linkedList.AddFront(1);
+
+            Assert.Equal(1, linkedList.GetFirst());
+        }
+
+        [Fact]
+        public void GetLast()
+        {
+            var linkedList = new LinkedList<int>();
+            linkedList.AddFront(1);
+
+            Assert.Equal(1, linkedList.GetLast());
+
+            linkedList.AddFront(2);
+            Assert.Equal(1, linkedList.GetLast());
+        }
+
+        [Fact]
+        public void AddBack()
+        {
+            var linkedList = new LinkedList<int>();
+
+            linkedList.AddBack(1);
+            linkedList.AddBack(2);
+            linkedList.AddBack(3);
+
+            Assert.Equal(1, linkedList.GetFirst());
+            Assert.Equal(3, linkedList.GetLast());
+        }
+
+        [Fact]
+        public void Size()
+        {
+            var linkedList = new LinkedList<int>();
+
+            Assert.Equal(0, linkedList.Size());
+
+            linkedList.AddFront(1);
+            linkedList.AddBack(2);
+
+            Assert.Equal(2, linkedList.Size());
+        }
+
+        [Fact]
+        public void Clear()
+        {
+            var linkedList = new LinkedList<int>();
+
+            linkedList.AddBack(1);
+            linkedList.AddBack(2);
+            linkedList.AddBack(3);
+
+            Assert.Equal(3, linkedList.Size());
+
+            linkedList.Clear();
+
+            Assert.Equal(0, linkedList.Size());
+        }
+
+        [Fact]
+        public void DeleteValue()
+        {
+            var linkedList = new LinkedList<int>();
+
+            linkedList.AddBack(1);
+            linkedList.AddBack(2);
+            linkedList.AddBack(3);
+
+            linkedList.DeleteValue(2);
+
+            Assert.Equal(2, linkedList.Size());
+            Assert.Equal(1, linkedList.GetFirst());
+            Assert.Equal(3, linkedList.GetLast());
+        }
+
+        [Fact]
+        public void DeleteLastValue()
+        {
+            var linkedList = new LinkedList<int>();
+
+            linkedList.AddBack(1);
+            linkedList.AddBack(2);
+            linkedList.AddBack(3);
+            linkedList.AddBack(4);
+
+            linkedList.DeleteValue(4);
+
+            Assert.Equal(3, linkedList.Size());
+            Assert.Equal(1, linkedList.GetFirst());
+            Assert.Equal(3, linkedList.GetLast());
+        }
+    }
+}
